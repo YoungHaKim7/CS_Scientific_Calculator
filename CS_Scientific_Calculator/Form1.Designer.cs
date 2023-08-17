796bf27915b8c540ab9d8814fe0b50b1a6ec2a8d
GYoung <ytok1108@aol.com>
2023-08-18 06:55:38 +0900

    초기 커밋.

---
+++ CS_Scientific_Calculator/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace CS_Scientific_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Arial", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox1.Location = new Point(12, 32);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(318, 59);
+            textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Arial Narrow", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(12, 97);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 80);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Arial", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(93, 97);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 80);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Arial", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(174, 97);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 80);
+            button3.TabIndex = 3;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Arial", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.Location = new Point(255, 97);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 80);
+            button4.TabIndex = 4;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(833, 647);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+    }
+}
